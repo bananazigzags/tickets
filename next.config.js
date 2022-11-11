e1d6e5ebf38cb4fa32962bd91fd6c6437324d2e3
Maria <m.tokareva@andersenlab.com>
2022-11-11 19:26:05 +0300

    feat: create main page

--- next.config.js
+++ next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
---
+++ pages/avia/index.tsx
@@ -0,0 +1,7 @@
+export default function Avia() {
+  return (
+    <div>
+      Avia
+    </div>
+  )
+}
